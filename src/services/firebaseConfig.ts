21aca0fcf7a4b02d9531c8f7e4595fa987011129
Gabriel silva <gabriel23teotonio@gmail.com>
2023-10-07 20:08:34 -0300

    refactor: create new folder for firebase config

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyBZNCaec_d_NEksICPhLs2AxjE64k4abOE",
+   authDomain: "avalia-4a968.firebaseapp.com",
+   projectId: "avalia-4a968",
+   storageBucket: "avalia-4a968.appspot.com",
+   messagingSenderId: "194487410132",
+   appId: "1:194487410132:web:388f25e8ef335aec908a41"
+ };
+
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
